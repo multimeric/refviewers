640305c4b70666305c3089a955664a88461474a7
Michael Milton <ttmigueltt@gmail.com>
2020-10-01 22:03:30 +1000

    Initial work

---
+++ src/App.js
@@ -0,0 +1,254 @@
+import React, {useMemo, useState} from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button';
+import Cite from 'citation-js';
+import MUIDataTable from 'mui-datatables';
+import {Grid} from '@material-ui/core';
+import sanitizeHtml from 'sanitize-html';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Michael Milton
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+/**
+ * Outputs CSL JSON from an arbitrary citation file
+ * @returns {Promise<*>}
+ */
+async function parseFile(file) {
+    const contents = await file.text();
+    const example = new Cite(contents);
+    return example.format('data', {format: 'object'});
+}
+
+class Author {
+    constructor(name) {
+        this.fullName = name;
+        this.authorships = [];
+        this.firstAuthorships = [];
+        this.lastAuthorships = [];
+    }
+
+    get numAuthorships() {
+        return this.authorships.length;
+    }
+
+    get textAuthorships() {
+        return this.authorships.join(', ');
+    }
+
+    get numFirstAuthorships() {
+        return this.firstAuthorships.length;
+    }
+
+    get textFirstAuthorships() {
+        return this.firstAuthorships.join(', ');
+    }
+
+    get numLastAuthorships() {
+        return this.lastAuthorships.length;
+    }
+
+    get textLastAuthorships() {
+        return this.lastAuthorships.join(', ');
+    }
+}
+
+/**
+ * Returns an ID used to key a dictionary, unique to a certain author
+ * @param author
+ */
+function authorId(author) {
+    const names = [];
+    if ('given' in author && author.given.length > 0) {
+        names.push(author.given.trim().split(' ')[0]);
+    }
+    if ('family' in author && author.family.length > 0) {
+        names.push(author.family.trim().split(' ')[0]);
+    }
+    return names.join('|');
+}
+
+/**
+ * Converts the citation file into an array of author objects
+ * @param citations
+ * @returns {any[]}
+ */
+function getAuthors(citations) {
+    const authors = new Map();
+    for (let work of citations) {
+        if (!Array.isArray(work.author)) {
+            continue;
+        }
+        const workTitle = sanitizeHtml(work.title, {
+            allowedTags: [],
+            allowedAttributes: {}
+        });
+        work.author.forEach((author, i) => {
+
+            const authorLookup = authorId(author);
+            let authorObj;
+            if (!authors.has(authorLookup)) {
+                authorObj = new Author(author.given + ' ' + author.family);
+                authors.set(authorLookup, authorObj);
+            }
+            else {
+                authorObj = authors.get(authorLookup);
+            }
+
+            authorObj.authorships.push(workTitle);
+            if (i === 0) {
+                authorObj.firstAuthorships.push(workTitle);
+            }
+            if (i === work.author.length - 1) {
+                authorObj.lastAuthorships.push(workTitle);
+            }
+
+        });
+    }
+
+    return Array.from(authors.values());
+}
+
+export default function App() {
+    const [file, setFile] = useState();
+    const [error, setError] = useState(null);
+    const authors = useMemo(() => {
+        if (file) {
+            return getAuthors(file);
+        }
+    }, [file]);
+
+    return (
+        <Container maxWidth="md">
+            <AppBar position="static">
+                <Toolbar>
+                    <Box flexGrow={1}>
+                        <Typography variant="h2">
+                            refviewers
+                        </Typography>
+                    </Box>
+                    <Button href="https://github.com/TMiguelT/refviewers" color="inherit">GitHub</Button>
+                </Toolbar>
+            </AppBar>
+            <Box my={4}>
+                <Grid container spacing={2} alignItems={'center'} direction={'column'}>
+                    <Grid item>
+                        <Typography variant="body1" component="h1" gutterBottom>
+                            refviewers is a simple browser utility for recommending reviewers for your manuscript, in
+                            case you are submitting a paper to a journal that requires this.
+                        </Typography>
+                        <Typography variant="body1" component="h1" gutterBottom>
+                            To use, simply upload a RIS, BibTeX or CSL-JSON file generated by your citation manager.
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            variant="contained"
+                            component="label"
+                        >
+                            Upload File
+                            <input
+                                type="file"
+                                style={{display: 'none'}}
+                                onChange={event => {
+                                    const fileType = event.currentTarget.files[0].type;
+                                    parseFile(event.currentTarget.files[0]).then(parsed => setFile(parsed)).catch(e => {
+                                        setError(`Invalid file type! You provided ${fileType}, which is not supported.`);
+                                    });
+                                }}
+                            />
+                        </Button>
+                    </Grid>
+                    <Grid item>
+                        {file &&
+                        <MUIDataTable
+                            title={'Authors'}
+                            data={authors}
+                            columns={[
+                                {
+                                    name: 'fullName',
+                                    label: 'Full Name',
+                                    options: {
+                                        sort: false,
+                                    }
+                                },
+                                {
+                                    name: 'textAuthorships',
+                                    label: 'All Authorships',
+                                    options: {
+                                        sort: false,
+                                    }
+                                },
+                                {
+                                    name: 'numAuthorships',
+                                    label: 'Total Authorships',
+                                    options: {
+                                        sort: true,
+                                        sortDirection: 'desc',
+                                    }
+                                },
+                                {
+                                    name: 'textFirstAuthorships',
+                                    label: 'First Authorships',
+                                    options: {
+                                        sort: false,
+                                        display: false
+                                    }
+                                },
+                                {
+                                    name: 'numFirstAuthorships',
+                                    label: 'Total First Authorships',
+                                    options: {
+                                        sort: true,
+                                    }
+                                },
+                                {
+                                    name: 'textLastAuthorships',
+                                    label: 'Last Authorships',
+                                    options: {
+                                        sort: false,
+                                        display: false
+                                    }
+                                },
+                                {
+                                    name: 'numLastAuthorships',
+                                    label: 'Total Last Authorships',
+                                    options: {
+                                        sort: true,
+                                    }
+                                },
+                            ]}
+                        />
+                        }
+                    </Grid>
+                    <Grid item>
+                        <Copyright/>
+                    </Grid>
+                </Grid>
+            </Box>
+            <Snackbar open={!!error} autoHideDuration={6000} onClose={() => setError(null)}>
+                <MuiAlert variant={'filled'} onClose={() => setError(null)} severity="error">
+                    {error || ''}
+                </MuiAlert>
+            </Snackbar>
+        </Container>
+    );
+}
