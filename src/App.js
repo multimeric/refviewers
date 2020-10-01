c88b935bb762537ee10fdfa7be7f3a0f3755a1d2
Michael Milton <ttmigueltt@gmail.com>
2020-10-01 22:20:33 +1000

    Fix warnings

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 function Copyright() {
     return (
